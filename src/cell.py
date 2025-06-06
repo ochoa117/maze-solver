8d5ad43c30d2638eacd9fe5b2a5c9f09897c5b83
ochoa117 <williamochoa96@gmail.com>
2025-06-06 12:23:25 -0700

    Update Maze and Cell classes and create tests

--- src/cell.py
+++ src/cell.py
@@ -1,7 +1,7 @@
 from graphics import Line, Point
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -20,16 +20,16 @@ class Cell:
         self.__x2 = x2
         self.__y2 = y2
         if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self.__win.draw_line(line)
         if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self.__win.draw_line(line)
         if self.has_right_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self.__win.draw_line(line)
         if self.has_bottom_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line)
     
     def draw_move(self, to_cell, undo=False):
--- src/maze.py
+++ src/maze.py
@@ -11,7 +11,7 @@ class Maze:
             num_cols,
             cell_size_x,
             cell_size_y,
-            win,
+            win=None,
     ):
         self.__x1 = x1
         self.__y1 = y1
