6140fb05c5711b2ac03e53877358c0bdbd7f7438
ochoa117 <williamochoa96@gmail.com>
2025-06-06 11:25:47 -0700

    Add Maze class with methods to draw maze on window

---
+++ src/maze.py
@@ -0,0 +1,48 @@
+from cell import Cell
+import random
+import time
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win,
+    ):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self.__create_cells()
+    
+    def __create_cells(self):
+        for i in range(self.__num_cols):
+            col_cells = []
+            for j in range(self.__num_rows):
+                col_cells.append(Cell(self.__win))
+            self.__cells.append(col_cells)
+        for i in range(self.__num_cols):
+            for j in range(self.__num_rows):
+                self.__draw_cell(i, j)
+    
+    def __draw_cell(self, i, j):
+        x1 = self.__cell_size_x * i + self.__x1
+        x2 = self.__cell_size_x + x1
+        y1 = self.__cell_size_y * j + self.__y1
+        y2 = self.__cell_size_y + y1
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self.__animate()
+    
+    def __animate(self):
+        if self.__win is None:
+            return
+        self.__win.redraw()
+        time.sleep(0.05)
